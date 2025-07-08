3f2015c077645d7b5afde4817d7dd1d1ed07cc5c
nesi04 <neeraj579singh@gmail.com>
2025-07-08 18:20:43 +0530

    changes that i made first

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Slot } from 'expo-router'
 
 export default function _Layout() {
   return (
-    <Slot></Slot>
+    <SafeAreaView>
+      <Text> Auth layout</Text>
+      <Slot/>
+    </SafeAreaView>
   )
 }
